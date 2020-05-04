cf50a508c8730b6328ddcf4050f81582efd4f057
Joel Ralph Balignasay <joelralph06@gmail.com>
2020-05-04 18:42:16 +0800

    covid-tracker

---
+++ client/src/actions/constants.js
@@ -0,0 +1,10 @@
+export const FETCH_CORONA_STATISTICS = 'FETCH_CORONA_STATISTICS';
+export const SHOW_COUNTRY_STATISTICS = 'SHOW_COUNTRY_STATISTICS';
+export const FETCH_MARKERS = 'FETCH_MARKERS';
+export const MAP_STYLE= 'MAP_STYLE';
+export const SET_ACTION = 'SET_ACTION';
+export const MAP_STYLE_ACTION = 'MAP_STYLE_ACTION';
+export const MAP_FLY_ACTION = 'MAP_FLY_ACTION';
+
+export const BASE_URL = `http://localhost:9000`;
+export const MAPBOX_ACCESS_TOKEN = `pk.eyJ1IjoibWFtb3lrbyIsImEiOiJjazkwc3FjeDUwNTJ4M21vMm1wb3Qwc2toIn0.lHMcKMOXSGbTvAnu2LzKCg`;
